86591e908cb43dbc9a20945d374423d48543b60d
Amoz19 <aungmon10co.com>
2023-11-22 12:56:10 +0630

    setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-blue-600 text-2xl font-black font-mono flex justify-center items-center h-screen">
+      React + Vite + Bun + Tailwind
+    </h1>
+  );
+}
+
+export default App;
